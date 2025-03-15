8933f825e3e353ea20d276bc2380dc4345555613
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-15 13:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function functionName() {
+	alert("functionName");
+}
+
+functionName();
+
+function functionName() {
+	alert(arguments.callee.name);
+}
+
+function AccioJob() {
+	alert(arguments.callee.name);
+}
+
+
+functionName();
+AccioJob();
